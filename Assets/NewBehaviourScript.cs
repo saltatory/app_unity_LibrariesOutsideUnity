2013ebea70630d4f5bd0bec683d6edb4fe6d0d4a
Saltatory <jeffrey.hohenstein@gmail.com>
2015-12-22 03:18:55 -0800

    Sample parent Unity project with a subproject for a library

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using TestLibrary;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ClassUnderTest c = new ClassUnderTest();
+        Debug.Log("Did it!");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
